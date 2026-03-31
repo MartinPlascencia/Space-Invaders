a9375d7a100b9fb5a247cd2bd5bda5516675d031
MartinPlascencia <martin.plascencia@live.com.mx>
2026-03-30 20:33:05 -0600

    Implementado Menu, y comienzo armas.

---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Events;
+using System.Collections;
+
+public class SceneController : MonoBehaviour
+{
+    [SerializeField]
+    private UnityEvent onSceneStart;
+    [SerializeField]
+    private Animator fadeAnimator;
+    private void Start()
+    {
+        onSceneStart?.Invoke();
+    }
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+    public void LoadSceneWithFade(string sceneName)
+    {
+        StartCoroutine(FadeCoroutine(sceneName));
+    }
+
+    private IEnumerator FadeCoroutine(string sceneName)
+    {
+        fadeAnimator.Play("FadeOut", 0, 0f);
+        yield return null;
+        yield return new WaitForSeconds(fadeAnimator.GetCurrentAnimatorStateInfo(0).length);
+        SceneManager.LoadScene(sceneName);
+    }
+}
